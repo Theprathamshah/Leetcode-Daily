e3338a88c2629a9f57696181ecb31db2da9d3d3d
Pratham Shah <itspratham2911@gmail.com>
2025-01-06 23:54:42 +0530

    feat: :sparkles: C++ solution for minOperations quesiton! january 6th 2024

---
+++ jan2025/minOperations.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+vector<int> minOperations(string boxes) {
+        vector<int>res;
+        int n = boxes.size();
+        for(int i = 0;i<n;i++) {
+            int sum = 0;
+            for(int j = 0;j<n;j++) {
+                if(i!=j and boxes[j]=='1') {
+                    sum+=abs(i-j);
+                }
+            }
+            res.push_back(sum);
+        }
+        return res;
+    }
+int main(int argc, char const *argv[])
+{
+    /* code */
+    string s;
+    cin>>s;
+    vector<int> res = minOperations(s);
+    for(int i = 0;i<res.size();i++) {
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
